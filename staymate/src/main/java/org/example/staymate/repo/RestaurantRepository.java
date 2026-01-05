63d5533c50b1fc048beb15077e4b585074ecdc35
yash_718 <thummaryash969@gmail.com>
2026-01-05 22:42:13 +0530

    Delete staymate directory
    "delete"

--- staymate/src/main/java/org/example/staymate/repo/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.staymate.repo;
-
-import org.example.staymate.model.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface RestaurantRepository
-        extends JpaRepository<Restaurant, Long> {
-
-    Restaurant findByPhone(String phone);
-}
